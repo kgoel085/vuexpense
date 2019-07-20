522fef1aab60dadd72cb1e0cb5e854738b30f2eb
kgoel085 <kgoel085@gmail.com>
2019-07-21 01:58:30 +0530

    Merged signup & login in one component

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,23 +3,33 @@
         <v-container fluid>
             <v-layout row wrap>
                 <v-flex xs12>
-                    <h1>Login</h1>
+                    <h1>{{ title }}</h1>
                 </v-flex>
             </v-layout>
         </v-container>
         <v-container>
             <v-layout row wrap>
                 <v-flex xs12>
-                    <v-text-field autofocus label="Username" v-model="formData.username.value" :counter="10" :rules="formData.username.rules"></v-text-field>
+                    <!-- <v-text-field autofocus label="Username" v-model="formData.username.value" :counter="10" :rules="formData.username.rules"></v-text-field> -->
                 </v-flex>
                 <v-flex xs12>
                     <v-text-field label="Email" v-model="formData.email.value" :rules="formData.email.rules"></v-text-field>
                 </v-flex>
-                <v-flex xs12>
+                <v-flex xs12 v-if="loginPg">
                     <v-text-field :type="formData.password.showPass ? 'text' : 'password'" label="Password" v-model="formData.password.value" :rules="formData.password.rules" @click:append="formData.password.showPass = !formData.password.showPass" :append-icon="formData.password.showPass ? 'visibility' : 'visibility_off'"></v-text-field>
                 </v-flex>
+                <v-flex xs12 v-else>
+                    <v-layout row wrap>
+                        <v-flex xs12 sm6 md6 lg6 xl6 class="pa-1">
+                            <v-text-field :type="formData.password.showPass ? 'text' : 'password'" label="Password" v-model="formData.password.value" :rules="formData.password.rules" @click:append="formData.password.showPass = !formData.password.showPass" :append-icon="formData.password.showPass ? 'visibility' : 'visibility_off'"></v-text-field>
+                        </v-flex>
+                        <v-flex xs12 sm6 md6 lg6 xl6 class="pa-1">
+                            <v-text-field :type="formData.confirmPassword.showPass ? 'text' : 'password'" label="Confirm Password" v-model="formData.confirmPassword.value" :rules="formData.confirmPassword.rules" @click:append="formData.confirmPassword.showPass = !formData.confirmPassword.showPass" :append-icon="formData.confirmPassword.showPass ? 'visibility' : 'visibility_off'"></v-text-field>
+                        </v-flex>
+                    </v-layout>
+                </v-flex>
                 <v-flex xs12 class="text-xs-center">
-                    <v-btn flat @click="valdiate" class="secondary" :disabled="!formData.valid">Login</v-btn>
+                    <v-btn flat @click="valdiate" class="secondary" :disabled="!formData.valid">{{ title }}</v-btn>
                 </v-flex>
             </v-layout>
         </v-container>
@@ -49,11 +59,25 @@ export default {
                     value: null,
                     showPass: false, 
                     rules:[
-                        v => !!v || 'Password is required'
+                        v => !!v || 'Password is required',
+                        v => (v && v.length > 6) || 'Password must be greater than 6 characters'
+                    ]
+                },
+                confirmPassword: {
+                    value: null, 
+                    showPass: false,
+                    rules:[
+                        v => (!!v && v) === this.formData.password.value || 'Password does not matches'
                     ]
                 },
                 valid: false
             }
+        }
+    },
+    computed:{
+        title(){
+            if(this.loginPg) return 'Login';
+            return 'Sign Up';
         }
     },
     methods:{
@@ -61,14 +85,49 @@ export default {
         valdiate(){
             // If form is validated
             if(this.$refs.loginForm.validate()) {
-                let formDt = {};
-                Object.keys(this.formData).forEach(field => formDt[field] = this.formData[field].value);
+                let email = this.formData.email.value;
+                let password = this.formData.password.value;
+
+                // Create / login user
+                this.manageUser(email, password);
 
                 return false;
             }
 
             alert('Form not valid. Please provide all the required fields');
             return false;
+        },
+        
+        // Logs user in
+        manageUser(email, pass){
+            // Set required function to be called
+            let auth = (this.loginPg) ? 'signInWithEmailAndPassword' : 'createUserWithEmailAndPassword';
+            this.$__firebase.fireauth[auth](email, pass).then(resp => {
+                this.$refs.loginForm.reset();
+
+                let routeName = 'home';
+
+                // If page is not login
+                if(!this.loginPg){
+                    routeName = 'login';
+
+                    // Sign out new user , default working of firebase
+                    this.$__firebase.fireauth.signOut();
+                }
+
+                // Push to the decided page / route
+                this.$router.push({name: routeName});
+
+            }).catch(err => {
+                console.log(err);
+            });
+            
+        }
+    },
+    props:{
+        loginPg:{
+            default: true,
+            type: Boolean
         }
     }
 }
