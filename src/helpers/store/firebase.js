6935d66f498ca46200f235cae27128d4fd78ffde
= <=>
2019-07-23 14:30:30 +0530

    Added default profile pic url

--- src/helpers/store/firebase.js
+++ src/helpers/store/firebase.js
@@ -2,7 +2,10 @@
 const firebase = {
     state: { 
         // States current auth status of the user
-        user: false
+        user: false,
+
+        // Default profile pic for every user
+        defaultProfile: 'https://api.adorable.io/avatars/400/abott@adorable'
     },
     mutations: {
         // Sets teh current auth state of the user
