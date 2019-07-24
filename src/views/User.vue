4862d9f9d1c223013325a06907bc09cf9beb0411
= <=>
2019-07-24 18:14:42 +0530

    Added user related data elements

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,10 +1,38 @@
 <template>
-    
+    <v-container>
+        <v-layout row wrap v-if="userData">
+            <v-flex xs12></v-flex>
+        </v-layout>
+        <loader v-else></loader>
+    </v-container>
 </template>
 
 <script>
-export default {
+import loader from '@/components/Loader'
 
+export default {
+    data(){
+        return {
+            userData: false,
+            currentUser: this.$__firebase.fireauth.currentUser
+        }
+    },
+    components:{
+        loader
+    },
+    methods:{
+        getData(){
+            let userId = this.currentUser.uid;
+            this.$__firebase.firestore.collection('users').doc(userId).get().then(doc => {
+                if(doc.exists) this.userData = doc.data();
+            }).catch(err => {
+                this.$store.commit('setSnackMsg', err.message);
+            });
+        }
+    },
+    mounted(){
+        this.getData();
+    }
 }
 </script>
 
