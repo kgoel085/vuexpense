492855b655ae603006e847e7308631707edad005
kgoel085 <kgoel085@gmail.com>
2019-07-28 22:00:08 +0530

    Corrected the user photo url in state

--- src/helpers/store/firebase.js
+++ src/helpers/store/firebase.js
@@ -14,6 +14,7 @@ const firebase = {
         // Sets the current auth state of the user
         setUser(state, usr = false){
             state.user = usr;
+            if(usr.hasOwnProperty('photoURL')) state.defaultProfile = usr.photoURL;
         },
 
         // Sets the current state of the signed up user
