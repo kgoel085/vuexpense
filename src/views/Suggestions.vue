ff9669701866e96f4c85aeae9bb20212236d656e
kgoel085 <kgoel085@gmail.com>
2019-08-02 00:10:34 +0530

    Under construction page

--- src/views/Suggestions.vue
+++ src/views/Suggestions.vue
@@ -1,5 +1,18 @@
 <template>
-    
+    <v-jumbotron
+    :gradient="gradient"
+    dark
+    src="https://cdn.vuetifyjs.com/images/parallax/material2.jpg"
+    height="100%"
+  >
+    <v-container fill-height>
+      <v-layout align-center>
+        <v-flex text-xs-center>
+          <h3 class="display-3">Coming Soon ... !</h3>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-jumbotron>
 </template>
 
 <script>
