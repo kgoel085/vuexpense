3b5897515cf42c00379ac85eced75a791d88ebab
kgoel085 <kgoel085@gmail.com>
2019-08-04 19:23:12 +0530

    Added data filter Added data updation for either new or updated records Added method to mark data as delete and to restore same

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -18,51 +18,105 @@
 					<v-tab-item key="showData">
 						<v-card flat>
 							<v-layout row wrap>
-								<!-- Show fetched data -->
-								<v-slide-x-transition>
-									<v-flex xs12 v-if="!loadingData && hasData">
-										<div class="d-flex justify-between align-center mb-3">
-											<v-btn @click="expandPanel = !expandPanel">{{ (expandPanel) ? 'Collapse' : 'Expand' }}</v-btn>
-										</div>
-										<v-expansion-panel v-model="panel" expand popout focusable :key="$store.state.global.updateView">
-											<v-expansion-panel-content v-for="(type, indx) in userData" :key="indx">
-												<template v-slot:header>
-													<v-layout row wrap class="text-capitalize">
-														<v-flex class="grow" :class="type.color+'--text'">
-															<span>{{ indx }}</span>
-														</v-flex>
-														<v-flex class="shrink" :class="type.color+'--text'">
-															<small>Total:</small>	  <strong>{{ type.total }}</strong>
-														</v-flex>
-													</v-layout>
-												</template>
-												<v-alert :value="true" type="info" v-if="type.data.length == 0" class="ma-2"> No expense data found </v-alert>
-												<v-card v-else>
-													<v-list-tile v-for="(data,indx) in type.data" :key="indx">
-														<v-list-tile-content>
-															<v-list-tile-title>
-																{{ data.title }} 
-															</v-list-tile-title>
-														</v-list-tile-content>
+								<!-- Data filter -->
+								<v-flex xs12>
+									<v-select class="shrink"
+										:items="filters"
+										label="Show Records"
+										v-model="showRecords"
+										item-text="name"
+										item-value="value"
+									></v-select>
+								</v-flex>
 
-														<v-list-tile-action>
-															<span>{{ data.value }} <v-btn small icon class="pa-2 ma-2" @click="rowUpdate = data"><v-icon size="20" class="px-2">edit</v-icon></v-btn></span>
-														</v-list-tile-action>
-													</v-list-tile>
-												</v-card>
-											</v-expansion-panel-content>
-										</v-expansion-panel>
-									</v-flex>
-									<v-flex xs12 v-else>
-										<loader v-if="loadingData"></loader>
-										<v-alert :value="true" type="info" v-else> No expense data found </v-alert>
-									</v-flex>
-								</v-slide-x-transition>
+								<!-- Show fetched data -->
+								<v-flex xs12 v-if="!loadingData && hasData">
+									<div class="d-flex justify-between align-center mb-1">
+										<v-btn @click="expandPanel = !expandPanel">{{ (expandPanel) ? 'Collapse' : 'Expand' }}</v-btn>
+									</div>
+									<v-expansion-panel v-model="panel" expand popout focusable :key="$store.state.global.updateView" :disabled="disableCalender">
+										<v-expansion-panel-content v-for="(type, indx) in userData" :key="indx">
+											<template v-slot:header>
+												<v-layout row wrap class="text-capitalize">
+													<v-flex class="grow" :class="type.color+'--text'">
+														<span>{{ indx }}</span>
+													</v-flex>
+													<v-flex class="shrink" :class="type.color+'--text'">
+														<small>Total:</small>	  <strong>{{ type.total }}</strong>
+													</v-flex>
+												</v-layout>
+											</template>
+											<v-alert :value="true" type="info" v-if="type.data.length == 0" class="ma-2"> No expense data found </v-alert>
+											<v-card v-else>
+												<v-list-tile v-for="(data,indx) in type.data" :key="indx" :class="(data.hasOwnProperty('delete') && data.delete == true) ? 'red--text strikethrough' : '' ">
+													<v-list-tile-content>
+														<v-list-tile-title>
+															<span class="delText">{{ data.title }}</span> 
+														</v-list-tile-title>
+													</v-list-tile-content>
+
+													<v-list-tile-action>
+														<span>
+															<span class="delText">{{ data.value }}</span>
+															<v-btn small icon class="ma-2" @click="rowUpdate = data">
+																<v-icon size="20" class="px-2">edit</v-icon>
+															</v-btn>
+
+															<v-btn small icon class="ma-2" @click="confirmDelete(data.id)">
+																<v-icon size="20" class="px-2">
+																	{{ (data.hasOwnProperty('delete') && data.delete == true) ?'restore' : 'delete' }}
+																</v-icon>
+															</v-btn>
+														</span>
+													</v-list-tile-action>
+												</v-list-tile>
+											</v-card>
+										</v-expansion-panel-content>
+									</v-expansion-panel>
+								</v-flex>
+								<v-flex xs12 v-else>
+									<loader v-if="loadingData"></loader>
+									<v-alert :value="true" type="info" v-else> No expense data found </v-alert>
+								</v-flex>
 							</v-layout>
 						</v-card>
 					</v-tab-item>
 				</v-tabs>
 			</v-flex>
+
+			<!-- Confirmation dialog -->
+			<v-dialog
+				v-model="dialog.show"
+				persistent
+				width="50%"
+			>
+				<v-card color="primary" dark v-if="dialog.loading || !dialog.data">
+					<v-card-text>
+						Please stand by
+						<v-progress-linear indeterminate color="white" class="mb-0" ></v-progress-linear>
+					</v-card-text>
+				</v-card>
+				<v-card v-else class="grow">
+					<v-card-title class="text-xs-center">
+						<v-flex xs12>
+							<span class="headline">Are you sure to {{ (dialog.restore ? 'restore' : 'delete') }} selected record ?</span>
+						</v-flex>
+					</v-card-title>
+
+					<v-card-actions class="text-xs-center">
+						<v-flex xs12>
+							<v-btn flat class="primary error" @click="deleteRecord(dialog.data.id, dialog.restore)">
+								Confirm
+							</v-btn>
+							<v-btn flat class="secondary" @click="dialog.show = false">
+								Cancel
+							</v-btn>
+						</v-flex>
+						
+					</v-card-actions>
+				</v-card>
+			</v-dialog>
+
 		</v-layout>
 	</v-container>
 </template>
@@ -70,6 +124,7 @@
 <script>
 	import loader from '@/components/Loader'
 	import expense from '@/components/User/Expense'
+
 	export default {
 		data(){
 			return {
@@ -86,6 +141,13 @@
 						color: 'success'
 					}
 				},
+
+				// Apply filters to data
+				filters:[
+					{name: 'All', value: 0}, 	// Show all records
+					{name: 'Active', value: 1}, 	// Show active records only
+					{name: 'Deleted', value: 2}		// Show deleted records only
+				],
 
 				// Annual calculations of expenses
 				annualData:{
@@ -118,7 +180,18 @@
 				disableCalender: false,
 
 				// Stores the data of row to be updated
-				rowUpdate:{}
+				rowUpdate:{},
+
+				// Manages the delete / restore  dialog box 
+				dialog:{
+					show: false,
+					restore: false,
+					loading: true,
+					data: false
+				},
+
+				// Which records to show
+				showRecords: 1
 			}
 		},
 		watch:{
@@ -140,6 +213,19 @@
 				if(val) this.panel = [...Object.keys(this.userData)].map(_ => true)
 				// Close all
 				else this.panel = []; 
+			},
+
+			// Reset dialog values
+			'dialog.show'(val){
+				if(!val){
+					this.dialog.data = false;
+					this.dialog.loading = true;
+				}
+			},
+
+			// Trigger new data whenever a new record type is requested
+			showRecords(val){
+				this.getData(true);
 			}
 
 		},
@@ -153,8 +239,15 @@
 				return this.$__firebase.fireauth.currentUser;
 			},
 
+			// Check whether any data is there or not
+			hasData(){
+				if(this.userData.debit.data.length > 0 || this.userData.credit.data.length > 0) return true;
+				return false;
+			}
+		},
+		methods:{
 			// User expense document
-			expenseDoc(){
+			expenseDoc(filter = true){
 
 				// Return the document instance, if available
 				if(this.userObj){
@@ -163,7 +256,25 @@
 
 					// Apply filter
 					let expenseDoc = this.$__firebase.firestore.collection('expenses').doc(this.userObj.uid).collection('data');
-					this.expenseDate.split('-').forEach((key, indx) => expenseDoc = expenseDoc.where(dtArr[indx], '==', parseInt(key)));
+
+					// If filter is required, only then apply it
+					if(filter){
+						this.expenseDate.split('-').forEach((key, indx) => expenseDoc = expenseDoc.where(dtArr[indx], '==', parseInt(key)));
+
+						// Record type filter ( Active , Deleted )
+						switch(this.showRecords){
+							// Show active records only
+							case 1:
+								expenseDoc = expenseDoc.where('delete', '==', false);
+							break;
+
+							// Show deleted records only
+							case 2:
+								expenseDoc = expenseDoc.where('delete', '==', true);
+							break;
+							
+						}
+					}
 
 					return expenseDoc;
 				}
@@ -171,13 +282,6 @@
 				return false;
 			},
 
-			// Check whether any data is there or not
-			hasData(){
-				if(this.userData.debit.data.length > 0 || this.userData.credit.data.length > 0) return true;
-				return false;
-			}
-		},
-		methods:{
 			// Reset the user data
 			resetData(){
 				// Reset main object
@@ -196,11 +300,11 @@
 
 			// Get the current date data
 			getData(refresh = false){
-				if(!this.expenseDoc) return false;
+				if(!this.expenseDoc()) return false;
 				if(refresh === true) this.resetData();
 
 				// Get data
-				this.expenseDoc.get().then(snapShot => {
+				this.expenseDoc().get().then(snapShot => {
 					snapShot.forEach(doc => {
 						if(doc.exists){
 							// Extract data
@@ -225,7 +329,14 @@
 				for(let key of keys){
 					let obj = (this.userData.hasOwnProperty(key) && this.userData[key].data.length > 0) ? this.userData[key].data : false;
 					if(obj){
-						obj.forEach(elem => {});
+						obj.forEach((elem, indx) => {
+							// If id already exists, remove that before adding it on the interface 
+							if(elem.id == id){
+								// Also subtract the total value
+								this.userData[key].total -= parseInt(elem.value);
+								this.$delete(this.userData[key].data, indx);
+							}
+						});
 					}
 				}
 				return false;
@@ -263,8 +374,8 @@
 			
 			// Listen for real time updates
 			updateData(){
-				if(!this.expenseDoc) return false;
-				this.expenseDoc.onSnapshot(snapshot => {
+				if(!this.expenseDoc()) return false;
+				this.expenseDoc().onSnapshot(snapshot => {
 					snapshot.forEach(doc => {
 						// Extract data
 						let data = doc.data();
@@ -275,6 +386,70 @@
 					});
 				}, function(err) {
 					this.$store.commit('setSnackMsg', err.message)
+				});
+			},
+
+			// Delete data
+			confirmDelete(id = false, restore = false){
+				const expenseCollection = this.expenseDoc(false);
+				if(!id || !expenseCollection) return false;
+
+				// Check if document really exists
+				const expenseDoc = expenseCollection.doc(id);
+				expenseDoc.get().then(doc => {	
+					if(doc.exists){
+						let data = doc.data();
+						data['id'] = id;
+						
+						// Check if record was previously deleted or not 
+						if(data.hasOwnProperty('delete')){
+							if(data.delete) this.dialog.restore = true;
+						}
+
+						// Set dialog values
+						this.dialog.show = true;
+						this.dialog.data = data;
+						this.dialog.loading = false;
+					}
+				}).catch(err => {
+					this.$store.commit('setSnackMsg', err.message);
+				});
+			},
+
+			// Mark the received document as deleted
+			deleteRecord(id, restore = false){
+				this.loading = true;
+
+				// Get collection
+				const expenseCollection = this.expenseDoc(false);
+				if(!id || !expenseCollection){
+					this.loading = false;
+					this.dialog.show = false;
+
+					this.$store.commit('setSnackMsg', 'Unknown Error !');
+					return false;
+				}
+
+				
+
+				// Get document
+				const expenseDoc = expenseCollection.doc(id);
+				let deleteVal = (restore) ? false : true;
+
+				// Perform operation
+				expenseDoc.update({
+					delete: deleteVal
+				}).then(resp => {
+					this.$store.commit('setSnackMsg', 'Operation successful');
+					
+					this.dialog.show = false;
+					this.loading = false;
+
+					// Refresh data
+					this.getData(true);
+				}).catch(err => {
+					this.loading = false;
+					this.$store.commit('setSnackMsg', err.message);
 				});
 			}
 		},
@@ -288,3 +463,8 @@
 		
 	}
 </script>
+
+<style scoped>
+	.strikethrough .delText{text-decoration: line-through;}
+</style>
+
