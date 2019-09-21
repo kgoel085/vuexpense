e0685e8c3ebd9ccb4f3d3144fb0e4066e6a57d2a
kgoel085 <kgoel085@gmail.com>
2019-09-22 01:30:22 +0530

    Added events to trigger to save user setting

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,11 +1,13 @@
 <template>
 	<v-container>
 		<v-layout row wrap>
+			<!-- Settings drop down -->
 			<v-flex xs12>
 				<v-select solo autofocus cache-items :items="dropDownItems" item-value="route" item-text="title" label="Select one" @input="triggerNav" v-model="currentRoute"></v-select>
 			</v-flex>
 
 			<v-flex xs12>
+				<!-- Settings router view -->
 				<router-view></router-view>
 			</v-flex>
 
@@ -49,19 +51,63 @@ export default {
 			set(val){
 				this.currentRouteVal = val;
 			}
+		},
+
+		// Current user
+		User(){
+			return this.$__firebase.fireauth.currentUser;
 		}
 	},
 	methods:{
 		// Load the selected settings
 		triggerNav(route = false){
 			this.$router.push({name: route});
+		},
+
+		// Save settings data
+		saveData(newData = {}){
+			const UserDoc = this.$__firebase.firestore.collection('settings').doc(this.User.uid);
+			if(!newData || typeof newData !== 'object'){
+				this.$store.commit('setSnackMsg', 'Invalid data provided');
+				return false;
+			}
+
+			const currentRouteName = this.currentRoute.split('.').pop().replace(/\b\w/g, l => l.toUpperCase());
+
+			UserDoc.get().then(doc => {
+				let userSettings = {};
+				if(doc.exists){
+					const data = doc.data();
+					if(data) userSettings = {...data};
+				}
+
+				if(Object.keys(userSettings).length > 0){
+					const updateData = {...userSettings, ...newData};
+					return UserDoc.set(updateData);
+				}
+
+				const err = new Error('No data found to update');
+				return Promise.reject(err);
+			}).then(result => {
+				this.$store.commit('setSnackMsg', `${currentRouteName} updated successfully`);
+				this.$router.push({name: 'settings'});
+			}).catch(err => {
+				this.$store.commit('setSnackMsg', err.message);
+			})
 		}
 	},
 	mounted(){
-		//console.log(this.$router);
 		EventBus.$on('Data_Changed', (stat = false) => {
 			this.dataChanged = stat;
 		});
+
+		// Capture data save event
+		EventBus.$on('SettingSaveData', (stat = {}) => {
+			this.saveData(stat)
+		})
+	},
+	beforeDestroy(){
+		EventBus.$off(['Data_Changed', 'SettingSaveData']);
 	}
 }
 </script>
